c12af007ff7dfd49d4ddd3fb9f649b7fc589090f
GildedSeraphim <nathanishaaz@gmail.com>
2025-03-08 10:34:17 -0600

    gitignored

--- build.zig
+++ build.zig
@@ -2,39 +2,28 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
     const exe = b.addExecutable(.{
-        .name = "zig-vk",
+        .name = "hello",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
-        .optimize = optimize,
+        .optimize = b.standardOptimizeOption(.{}),
     });
 
+    // ### Libraries ###
+    exe.linkSystemLibrary("glfw");
+    exe.linkSystemLibrary("vulkan");
+    exe.linkSystemLibrary("dl");
+    exe.linkSystemLibrary("pthread");
+    exe.linkSystemLibrary("X11");
+    exe.linkSystemLibrary("Xxf86vm");
+    exe.linkSystemLibrary("Xrandr");
+    exe.linkSystemLibrary("Xi");
+    // #################
+
+    exe.linkLibCpp();
     b.installArtifact(exe);
 
-    exe.linkSystemLibrary("vulkan");
-    exe.linkSystemLibrary("glfw");
-    exe.linkLibC();
-
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-    const test_step = b.step("test", "Run unit tests");
-
-    test_step.dependOn(&run_exe_unit_tests.step);
+    const run_exe = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run the application");
+    run_step.dependOn(&run_exe.step);
 }
