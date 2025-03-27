1d3c498a48a0286d2bd611776be4eade673ffb4a
GildedSeraphim <nathanishaaz@gmail.com>
2025-03-26 21:24:24 -0500

    working glfw window

--- src/main.zig
+++ src/main.zig
@@ -2,5 +2,28 @@ const std = @import("std");
 const c = @import("clibs.zig");
 
 pub fn main() !void {
-    std.debug.print("Hello World!", .{});
+    if (c.glfwInit() == 0) {
+        std.debug.print("Failed to initialize GLFW\n", .{});
+        return error.GLFWInitializationFailed;
+    }
+    defer c.glfwTerminate();
+
+    const window: ?*c.GLFWwindow = c.glfwCreateWindow(800, 600, "Vulkan Window", null, null);
+
+    if (window == null) {
+        std.debug.print("Failed to create GLFW window\n", .{});
+        return error.GLFWWindowCreationFailed;
+    }
+
+    var extension_count: u32 = 0;
+    _ = c.vkEnumerateInstanceExtensionProperties(null, &extension_count, null);
+
+    std.debug.print("{d} extensions supported\n", .{extension_count});
+
+    while (c.glfwWindowShouldClose(window) == 0) {
+        _ = c.glfwPollEvents();
+    }
+
+    c.glfwDestroyWindow(window);
+    std.debug.print("Window destroyed.\n", .{});
 }
