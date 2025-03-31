25656669f277ef9befb937440d7600bb560e3c3a
GildedSeraphim <nathanishaaz@gmail.com>
2025-03-31 11:52:28 -0500

    working window kinda wayland moment

--- src/main.zig
+++ src/main.zig
@@ -3,27 +3,53 @@ const c = @import("clibs.zig");
 
 pub fn main() !void {
     if (c.glfwInit() == 0) {
-        std.debug.print("Failed to initialize GLFW\n", .{});
         return error.GLFWInitializationFailed;
     }
     defer c.glfwTerminate();
+    _ = c.glfwWindowHint(c.GLFW_CLIENT_API, c.GLFW_NO_API);
+    _ = c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 1);
+    _ = c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 0);
 
     const window: ?*c.GLFWwindow = c.glfwCreateWindow(800, 600, "Vulkan Window", null, null);
 
     if (window == null) {
-        std.debug.print("Failed to create GLFW window\n", .{});
         return error.GLFWWindowCreationFailed;
     }
 
-    var extension_count: u32 = 0;
-    _ = c.vkEnumerateInstanceExtensionProperties(null, &extension_count, null);
+    _ = c.glfwMakeContextCurrent(window);
 
-    std.debug.print("{d} extensions supported\n", .{extension_count});
+    var extension_count: u32 = 0;
+    const extensions: [*c][*c]const u8 = c.glfwGetRequiredInstanceExtensions(&extension_count);
+
+    const appInfo = c.VkApplicationInfo{
+        .sType = c.VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        .pApplicationName = "Vulkan Window",
+        .applicationVersion = c.VK_MAKE_VERSION(1, 0, 0),
+        .pEngineName = "No Engine",
+        .engineVersion = c.VK_MAKE_VERSION(1, 0, 0),
+        .apiVersion = c.VK_API_VERSION_1_4,
+    };
+
+    const createInfo = c.VkInstanceCreateInfo{
+        .sType = c.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        .pApplicationInfo = &appInfo,
+        .enabledExtensionCount = extension_count,
+        .ppEnabledExtensionNames = extensions,
+    };
+
+    var instance: c.VkInstance = undefined;
+    if (c.vkCreateInstance(&createInfo, null, &instance) != c.VK_SUCCESS) {
+        return error.failedToCreateVulkanInstance;
+    }
+
+    var surface: c.VkSurfaceKHR = undefined;
+    if (c.glfwCreateWindowSurface(instance, window, null, &surface) != c.VK_SUCCESS) {
+        return error.failedToCreateWindowSurface;
+    }
+    defer c.vkDestroySurfaceKHR(instance, surface, null);
+    defer c.vkDestroyInstance(instance, null);
 
     while (c.glfwWindowShouldClose(window) == 0) {
         _ = c.glfwPollEvents();
     }
-
-    c.glfwDestroyWindow(window);
-    std.debug.print("Window destroyed.\n", .{});
 }
