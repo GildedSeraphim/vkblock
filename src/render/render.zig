fc13d539e37c2bc7cb543fee0a99236298d13fde
GildedSeraphim <nathanishaaz@gmail.com>
2025-04-01 19:07:50 -0500

    instance creation

--- src/render/render.zig
+++ src/render/render.zig
@@ -1,9 +1,15 @@
 const std = @import("std");
 const c = @import("../clibs.zig");
 const win = @import("window.zig");
+const vulkan = @import("vulkan.zig");
 
 pub const App = struct {
     pub fn mainLoop() !void {
+        // Vulkan Initialization
+        const vk = try vulkan.Vulk.initVulkan();
+        defer vk.cleanup();
+
+        // GLFW Initialization
         const w: win.Window = try win.Window.initWindow();
         defer c.glfwDestroyWindow(w.window);
         defer c.glfwTerminate();
